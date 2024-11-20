2a8e7d0cdd2ee384cd864e1939ef13ee9102d15e
MantasZup123 <mantaszup@gmail.com>
2024-11-20 20:58:44 +0200

    added immutable url

--- mantas_z_mod1_atsiskaitymas/eurovaistine_crawler.py
+++ mantas_z_mod1_atsiskaitymas/eurovaistine_crawler.py
@@ -1,7 +1,8 @@
 import requests
 from lxml.etree import HTML
 
-def crawl_eurovaistine(url: str):
+def crawl_eurovaistine():
+    url = "https://www.eurovaistine.lt/?utm_source=google&utm_medium=cpc&utm_campaign=%5BEV%20BRAND%5D%20-%20Eurovaistine&utm_id=1543031168&gbraid=0AAAAADRyq90KMHdmqbBcDjCO3L_MPjcFR&gclid=CjwKCAiArva5BhBiEiwA-oTnXR3GzX33oBWvrvMvSA4dtonpaovb2NwxqzrsJ44CFAvSJsjOLQrcehoCJsYQAvD_BwE"
     response = requests.get(url)
     text = response.text
     tree = HTML(text)
