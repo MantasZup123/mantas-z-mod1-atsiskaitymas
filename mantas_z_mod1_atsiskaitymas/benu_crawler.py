cb4ca92099d0ec40b307b9658d36bceecf4b945e
MantasZup123 <mantaszup@gmail.com>
2024-11-20 21:01:13 +0200

    added crwal_benu function and immutable url

---
+++ mantas_z_mod1_atsiskaitymas/benu_crawler.py
@@ -0,0 +1,5 @@
+import requests
+from lxml.etree import HTML
+
+def crawl_benu():
+    url = "https://www.benu.lt/?gad_source=1&gbraid=0AAAAAozt4RRMQ8axlxUuHlyNCvZBMrx27&gclid=CjwKCAiArva5BhBiEiwA-oTnXX6Umbk0M9UK8rG2G6UbnWgJeXX6HH_C4oWWtSg4vIZXSEvkmGTTPxoCZvgQAvD_BwE"
