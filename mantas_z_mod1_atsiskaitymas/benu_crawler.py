2d052e4064c044013bbbc147e836c096d4f8683c
MantasZup123 <mantaszup@gmail.com>
2024-11-20 21:06:15 +0200

    added variable products

--- mantas_z_mod1_atsiskaitymas/benu_crawler.py
+++ mantas_z_mod1_atsiskaitymas/benu_crawler.py
@@ -2,4 +2,13 @@ import requests
 from lxml.etree import HTML
 
 def crawl_benu():
-    url = "https://www.benu.lt/?gad_source=1&gbraid=0AAAAAozt4RRMQ8axlxUuHlyNCvZBMrx27&gclid=CjwKCAiArva5BhBiEiwA-oTnXX6Umbk0M9UK8rG2G6UbnWgJeXX6HH_C4oWWtSg4vIZXSEvkmGTTPxoCZvgQAvD_BwE"
+    url = "https://www.benu.lt/menesio-top-pasiulymai?vars/gclid/CjwKCAiArva5BhBiEiwA-oTnXX6Umbk0M9UK8rG2G6UbnWgJeXX6HH_C4oWWtSg4vIZXSEvkmGTTPxoCZvgQAvD_BwE"
+    response = requests.get(url)
+    text = response.text
+    tree = HTML(text)
+
+    products = tree.xpath(".//div[contains(@class, 'bnProductCard')]")
+
+    return [{
+
+    } for product in products][:4]
