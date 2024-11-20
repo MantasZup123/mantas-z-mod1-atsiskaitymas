aff21692db461b69b940e5daeee87462623f2a23
MantasZup123 <mantaszup@gmail.com>
2024-11-20 21:30:59 +0200

    changed the url

--- mantas_z_mod1_atsiskaitymas/eurovaistine_crawler.py
+++ mantas_z_mod1_atsiskaitymas/eurovaistine_crawler.py
@@ -2,7 +2,7 @@ import requests
 from lxml.etree import HTML
 
 def crawl_eurovaistine():
-    url = "https://www.eurovaistine.lt/?utm_source=google&utm_medium=cpc&utm_campaign=%5BEV%20BRAND%5D%20-%20Eurovaistine&utm_id=1543031168&gbraid=0AAAAADRyq90KMHdmqbBcDjCO3L_MPjcFR&gclid=CjwKCAiArva5BhBiEiwA-oTnXR3GzX33oBWvrvMvSA4dtonpaovb2NwxqzrsJ44CFAvSJsjOLQrcehoCJsYQAvD_BwE"
+    url = "https://www.eurovaistine.lt/kosmetika"
     response = requests.get(url)
     text = response.text
     tree = HTML(text)
