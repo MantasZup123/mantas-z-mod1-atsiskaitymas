425ba05520c501d5c063cb7bd75129b809a713b9
MantasZup123 <mantaszup@gmail.com>
2024-11-21 20:26:31 +0200

    added docstring to function crawl_benu

--- mantas_z_mod1_atsiskaitymas/benu_crawler.py
+++ mantas_z_mod1_atsiskaitymas/benu_crawler.py
@@ -1,9 +1,14 @@
 import requests
 from lxml.etree import HTML
 
-def crawl_benu():
-    url = "https://www.benu.lt/menesio-top-pasiulymai?vars/gclid/CjwKCAiArva5BhBiEiwA-oTnXX6Umbk0M9UK8rG2G6UbnWgJeXX6HH_C4oWWtSg4vIZXSEvkmGTTPxoCZvgQAvD_BwE"
-    response = requests.get(url)
+def crawl_benu(source):
+    """
+    Function reads data from benu website
+    :param source: Websites URL
+    :return: Data received - titles and prices
+    """
+    source = "https://www.benu.lt/menesio-top-pasiulymai?vars/gclid/CjwKCAiArva5BhBiEiwA-oTnXX6Umbk0M9UK8rG2G6UbnWgJeXX6HH_C4oWWtSg4vIZXSEvkmGTTPxoCZvgQAvD_BwE"
+    response = requests.get(source)
     text = response.text
     tree = HTML(text)
 
