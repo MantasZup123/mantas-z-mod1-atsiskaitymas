6bf5bd0cbe945be7f25a266fc5d4c36481ecfa11
MantasZup123 <mantaszup@gmail.com>
2024-11-21 19:02:31 +0200

    made if statements for url and correct functions

--- mantas_z_mod1_atsiskaitymas/crawler.py
+++ mantas_z_mod1_atsiskaitymas/crawler.py
@@ -3,6 +3,16 @@ import time
 from eurovaistine_crawler import crawl_eurovaistine
 from benu_crawler import crawl_benu
 
-def crawl(time_limit: int = 60, source: str, data_format: str = "json"):
+def crawl(time_limit: int = 60, url: str, data_format: str = "json"):
+    start_time = time.time()
+
+    if url == "eurovaistine":
+        data = crawl_eurovaistine()
+    elif url == "benu":
+        data = crawl_benu()
+    else:
+        raise ValueError("Tokios svetainės nucrawlinti negalime")
+
+
 
 
