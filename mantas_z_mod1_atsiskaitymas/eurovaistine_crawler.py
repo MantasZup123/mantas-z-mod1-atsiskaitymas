b900c4cc5484ee657d28baf4144c9c25abef7254
MantasZup123 <mantaszup@gmail.com>
2024-11-21 20:25:46 +0200

    added docstring to function crawl_eurovaistine

--- mantas_z_mod1_atsiskaitymas/eurovaistine_crawler.py
+++ mantas_z_mod1_atsiskaitymas/eurovaistine_crawler.py
@@ -1,9 +1,14 @@
 import requests
 from lxml.etree import HTML
 
-def crawl_eurovaistine():
-    url = "https://www.eurovaistine.lt/kosmetika"
-    response = requests.get(url)
+def crawl_eurovaistine(source):
+    """
+    Function reads data from eurovaistine website
+    :param source: Websites URL
+    :return: Data received - titles and prices
+    """
+    source = "https://www.eurovaistine.lt/kosmetika"
+    response = requests.get(source)
     text = response.text
     tree = HTML(text)
 
