ed2c543478008111a5d9f4f86a1c5fd3c0fdd109
MantasZup123 <mantaszup@gmail.com>
2024-11-21 20:31:28 +0200

    added docstring to function crawl

--- mantas_z_mod1_atsiskaitymas/crawler.py
+++ mantas_z_mod1_atsiskaitymas/crawler.py
@@ -5,12 +5,20 @@ import time
 from eurovaistine_crawler import crawl_eurovaistine
 from benu_crawler import crawl_benu
 
-def crawl(time_limit: int = 60, url: str, data_format: str = "json"):
+def crawl(source: str, time_limit: int = 60, data_format: str = "json"):
+    """
+    Function read website date and returns it in desired formats.
+
+    :param source: Takes websites URL from which read data.
+    :param time_limit: Time limit in seconds. After the certain time function stops working.
+    :param data_format: Data return format. Can be json, csv, dict.
+    :return: Data is formated in desired format.
+    """
     start_time = time.time()
 
-    if url == "eurovaistine":
+    if source == "eurovaistine":
         data = crawl_eurovaistine()
-    elif url == "benu":
+    elif source == "benu":
         data = crawl_benu()
     else:
         raise ValueError("Neturime prieigos prie šios svetainės.")
