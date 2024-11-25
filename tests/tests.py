50df7b0057aa1d461b0c820a094f3db667d1d8cb
MantasZup123 <mantaszup@gmail.com>
2024-11-25 19:03:24 +0200

    tested function crawl_gintarine

--- tests/tests.py
+++ tests/tests.py
@@ -2,3 +2,17 @@ import unittest
 from mantas_z_mod1_atsiskaitymas.crawler import crawl
 
 class TestCrawl(unittest.TestCase):
+
+    def test_crawl_gintarine(self):
+        source = "gintarine"
+        result = crawl(source, data_format="list")
+        self.assertEqual(list(result[0].keys()), ["Title", "Price"])
+
+    def test_crawl_benu(self):
+        source = "benu"
+        result = crawl(source, data_format="list")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
