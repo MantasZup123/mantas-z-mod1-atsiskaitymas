a10de527a67e9975d68ca1f1cc3fb4788368f37e
MantasZup123 <mantaszup@gmail.com>
2024-11-25 18:55:34 +0200

    crawled the titles and prices from url

--- mantas_z_mod1_atsiskaitymas/gintarine_crawler.py
+++ mantas_z_mod1_atsiskaitymas/gintarine_crawler.py
@@ -7,16 +7,16 @@ def crawl_gintarine():
     :param source: Websites URL
     :return: Data received - titles and prices
     """
-    source = "https://www.eurovaistine.lt/kosmetika"
+    source = "https://www.gintarine.lt/arbatos"
     response = requests.get(source)
     text = response.text
     tree = HTML(text)
 
-    products = tree.xpath("//a[contains(@class, 'productCard')]")
+    products = tree.xpath("//div[contains(@class, 'product-item')]")
 
     return[{
-        "Title": product.xpath(".//div[contains(@class, 'title')]/span/text()")[0].strip(),
-        "Price": product.xpath(".//div[contains(@class, 'newProductPrice')]/span/text()")[0].strip()
+        "Title": product.xpath(".//div[contains(@class, 'product__title')]/a/text()")[0].strip(),
+        "Price": product.xpath(".//div[contains(@class, 'product__price')]/span[contains(@class, 'product__price--regular')]/text()")[0].strip()
     } for product in products]
 
-#print(crawl_eurovaistine())
+print(crawl_gintarine())
